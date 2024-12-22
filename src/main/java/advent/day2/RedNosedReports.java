d2ddaf33b4c472e0c624dd76624982afe6cac226
Krzysztof Ziomek <kziomek@onet.eu>
2024-12-22 16:26:46 +0000

    AdventOfCode 2024 - day 2 - part 2

--- src/main/java/advent/day2/RedNosedReports.java
+++ src/main/java/advent/day2/RedNosedReports.java
@@ -4,7 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.function.Predicate;
 
+/**
+ * In Part2 we use index j to skip bad level.
+ * To cover edge case bad level is first or last element (initial pos of i) we process table from two directions.
+ * <p>
+ * Other way of solving it would be preprocessing array to remove one level from report before calling isReportSafe, but it would be slower as well.
+ * <p>
+ * There is room for deduplicating code.
+ */
 public class RedNosedReports {
 
     public static void main(String[] args) throws IOException {
@@ -29,13 +38,13 @@ public class RedNosedReports {
     }
 
     private static boolean isReportSafe(Integer[] report) {
-        return testDesc(report)
-            || testDescReverse(report)
-            || testAsc(report)
-            || testAscReverse(report);
+        return testForward(report, difference -> difference < 1 || difference > 3)
+            || testForward(report, difference -> difference < -3 || difference > -1)
+            || testBackward(report, difference -> difference < 1 || difference > 3)
+            || testBackward(report, difference -> difference < -3 || difference > -1);
     }
 
-    private static boolean testDescReverse(Integer[] report) {
+    private static boolean testBackward(Integer[] report, Predicate<Integer> predicate) {
         int badLevelCountReverse = 0;
         int i;
         int j;
@@ -43,7 +52,7 @@ public class RedNosedReports {
         j = i - 1;
         while (j >= 0) {
             int difference = report[i] - report[j];
-            if (difference < 1 || difference > 3) {
+            if (predicate.test(difference)) {
                 badLevelCountReverse++;
                 j--;
                 continue;
@@ -54,12 +63,12 @@ public class RedNosedReports {
         return badLevelCountReverse <= 1;
     }
 
-    private static boolean testDesc(Integer[] report) {
+    private static boolean testForward(Integer[] report, Predicate<Integer> predicate) {
         int badLevelCount = 0;
         int i = 0, j = 1;
         while (j <= report.length - 1) {
             int difference = report[i] - report[j];
-            if (difference < 1 || difference > 3) {
+            if (predicate.test(difference)) {
                 badLevelCount++;
                 j++;
                 continue;
@@ -68,41 +77,6 @@ public class RedNosedReports {
             j++;
         }
         return badLevelCount <= 1;
-    }
-
-    private static boolean testAsc(Integer[] report) {
-        int badLevelCount = 0;
-        int i = 0, j = 1;
-        while (j <= report.length - 1) {
-            int difference = report[i] - report[j];
-            if (difference < -3 || difference > -1) {
-                badLevelCount++;
-                j++;
-                continue;
-            }
-            i = j;
-            j++;
-        }
-        return badLevelCount <= 1;
-    }
-
-    private static boolean testAscReverse(Integer[] report) {
-        int badLevelCountReverse = 0;
-        int i;
-        int j;
-        i = report.length - 1;
-        j = i - 1;
-        while (j >= 0) {
-            int difference = report[i] - report[j];
-            if (difference < -3 || difference > -1) {
-                badLevelCountReverse++;
-                j--;
-                continue;
-            }
-            i = j;
-            j--;
-        }
-        return badLevelCountReverse <= 1;
     }
 
     public static void print(Integer[] report) {
