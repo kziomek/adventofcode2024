aed06c6ae7a91d7847489417264d119aa68e8305
Krzysztof Ziomek <kziomek@onet.eu>
2025-02-01 14:56:05 +0000

    AdventOfCode 2024 - day 8 part 1

--- src/main/java/advent/day8/DiskFragmenter.java
+++ src/main/java/advent/day8/DiskFragmenter.java
@@ -7,61 +7,62 @@ import java.nio.file.Path;
 public class DiskFragmenter {
 
     public static void main(String[] args) throws IOException {
-        String input = Files.readAllLines(Path.of("src/main/resources/day8/example.txt")).get(0);
-        //        String input = Files.readAllLines(Path.of("src/main/resources/day8/my-input.txt")).get(0);
+//        String input = Files.readAllLines(Path.of("src/main/resources/day8/example.txt")).get(0);
+                String input = Files.readAllLines(Path.of("src/main/resources/day8/my-input.txt")).get(0);
         System.out.println(input);
 
-        char[] arr = decompressInput(input);
+        int[] arr = decompressInput(input);
+
         compact(arr);
         System.out.println(arr);
-        int result = calculateChecksum(arr);
+        long result = calculateChecksum(arr);
         System.out.println("Part 1 result " + result);
     }
 
-    private static int calculateChecksum(char[] arr) {
-        int sum = 0;
+    private static long calculateChecksum(int[] arr) {
+        long sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == '.') {
+            if (arr[i] == -1) {
                 break;
             }
-            sum += i * Character.getNumericValue(arr[i]);
+            sum += (long)i * arr[i];
         }
         return sum;
     }
 
-    private static void compact(char[] arr) {
+    private static void compact(int[] arr) {
         int i = 0;
         int j = arr.length - 1;
         while (i < j) {
-            if (arr[i] != '.') {
+            if (arr[i] != -1) {
                 i++;
                 continue;
             }
-            if (arr[j] == '.') {
+            if (arr[j] == -1) {
                 j--;
                 continue;
             }
             arr[i] = arr[j];
-            arr[j] = '.';
+            arr[j] = -1;
             i++;
             j--;
         }
     }
 
-    private static char[] decompressInput(String input) {
+    private static int[] decompressInput(String input) {
         int size = calculateSize(input);
         System.out.println(size);
-        char[] arr = new char[size];
+        int[] arr = new int[size];
         int idx = 0;
         int id = 0;
         boolean isFile = true;
         for (char c : input.toCharArray()) {
-            int value = Character.getNumericValue(c);
-            for (int i = 0; i < value; i++) {
+            int blockSize = Character.getNumericValue(c);
+            for (int i = 0; i < blockSize; i++) {
                 if (isFile) {
-                    arr[idx] = Character.forDigit(id, 10);
+                    arr[idx] = id;
                 } else {
-                    arr[idx] = '.';
+                    arr[idx] = -1;
                 }
                 idx++;
             }
