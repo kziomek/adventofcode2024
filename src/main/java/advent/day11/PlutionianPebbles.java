c2c89c3f2da9b8218cf48d5b6ba66bad7aa1eec9
Krzysztof Ziomek <kziomek@onet.eu>
2025-02-08 16:21:59 +0000

    AdventOfCode 2024 - day 11 part2

--- src/main/java/advent/day11/PlutionianPebbles.java
+++ src/main/java/advent/day11/PlutionianPebbles.java
@@ -6,45 +6,77 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.*;
 
 public class PlutionianPebbles {
 
     public static void main(String[] args) throws IOException {
-        String line = Files.readAllLines(Path.of("src/main/resources/day11/example.txt")).getFirst();
-        //        String line = Files.readAllLines(Path.of("src/main/resources/day11/my-input.txt")).getFirst();
-        List<Long> stones = Arrays.stream(line.split(" ")).map(Long::valueOf).toList();
+//        String line = Files.readAllLines(Path.of("src/main/resources/day11/example.txt")).getFirst();
+                String line = Files.readAllLines(Path.of("src/main/resources/day11/my-input.txt")).getFirst();
+        List<String> stones = Arrays.stream(line.split(" ")).toList();
 
-        for (int i = 0; i < 25; i++) {
-            System.out.println(i);
-            stones = blink(stones);
-            //            System.out.println(stones);
-        }
-        System.out.println("result " + stones.size());
+        Map<String, Long> stoneMap = toMap(stones);
+        System.out.println(stoneMap);
+
+        Map<String, Long> blinkedStoneMap = blinkStoneMap(stoneMap, 75);
+        long count = count(blinkedStoneMap);
+        System.out.println("result " + count);
     }
 
-    private static List<Long> blink(List<Long> stones) {
-        List<Long> newStage = new ArrayList<>();
-        for (Long stone : stones) {
-            if (stone == 0) {
-                newStage.add(1L);
-                continue;
-            }
-            String s = stone.toString();
-            if (s.length() % 2 == 0) {
-                Long left = Long.valueOf(s.substring(0, s.length() / 2));
-                Long right = Long.valueOf(s.substring(s.length() / 2));
-                newStage.add(left);
-                newStage.add(right);
-            } else {
-                newStage.add(2024 * stone);
-            }
-        }
-
-        return newStage;
+    private static long count(Map<String, Long> blinkedStoneMap) {
+        return blinkedStoneMap.values().stream().reduce(Long::sum).get();
     }
 
-    public static long blink(long num, int times) {
-        return 0;
+    private static Map<String, Long> blinkStoneMap(Map<String, Long> stoneMap, int blinks) {
+        Map<String, Long> currStoneMap = stoneMap;
+        for (int j = 0; j < blinks; j++) {
+            Map<String, Long> newStage = new HashMap<>();
+            for (Map.Entry<String, Long> entry : currStoneMap.entrySet()) {
+                long count = entry.getValue();
+                List<String> blink = blink(entry.getKey());
+                for (String stone : blink) {
+                    if (!newStage.containsKey(stone)) {
+                        newStage.put(stone, 0L);
+                    }
+                    newStage.put(stone, newStage.get(stone) + count);
+                }
+            }
+            currStoneMap = newStage;
+            System.out.println(newStage);
+        }
+        return currStoneMap;
+    }
+
+    private static Map<String, Long> toMap(List<String> stones) {
+        Map<String, Long> map = new HashMap<>();
+        for (String stone : stones) {
+            if (!map.containsKey(stone)) {
+                map.put(stone, 0L);
+            }
+            map.put(stone, map.get(stone) + 1);
+        }
+        return map;
+    }
+
+    private static List<String> blink(String stone) {
+        List<String> newStones = new ArrayList<>();
+        if (stone.equals("0")) {
+            newStones.add("1");
+            return newStones;
+        }
+        if (stone.length() % 2 == 0) {
+            String left = stone.substring(0, stone.length() / 2);
+            String right = Long.toString(Long.parseLong(stone.substring(stone.length() / 2)));
+            newStones.add(left);
+            newStones.add(right);
+        } else {
+            newStones.add("" + (2024 * Long.parseLong(stone)));
+        }
+
+        return newStones;
     }
 }
