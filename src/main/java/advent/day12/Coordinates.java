6bc01e8d12dc643cf5de05902d1d6ac8e371f2a6
Krzysztof Ziomek <kziomek@onet.eu>
2025-02-15 14:44:36 +0000

    AdventOfCode 2024 - day 12 part1 working code

---
+++ src/main/java/advent/day12/Coordinates.java
@@ -0,0 +1,35 @@
+package advent.day12;
+
+import java.util.Objects;
+
+public class Coordinates {
+    int i;
+    int j;
+
+    public Coordinates(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Coordinates that = (Coordinates) o;
+        return i == that.i && j == that.j;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(i, j);
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates{" +
+            "i=" + i +
+            ", j=" + j +
+            '}';
+    }
+}
--- src/main/java/advent/day12/GardenGroups.java
+++ src/main/java/advent/day12/GardenGroups.java
@@ -4,27 +4,125 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class GardenGroups {
 
     public static void main(String[] args) throws IOException {
 
-//        char[][] gardenGrid = Files.readAllLines(Path.of("src/main/resources/day12/example1.txt"))
-        char[][] gardenGrid = Files.readAllLines(Path.of("src/main/resources/day12/example2.txt"))
+//                char[][] gardenGrid = Files.readAllLines(Path.of("src/main/resources/day12/example1.txt"))
+//        char[][] gardenGrid = Files.readAllLines(Path.of("src/main/resources/day12/example2.txt"))
+//        char[][] gardenGrid = Files.readAllLines(Path.of("src/main/resources/day12/example3.txt"))
+        char[][] gardenGrid = Files.readAllLines(Path.of("src/main/resources/day12/my-example.txt"))
             .stream().map(String::toCharArray).toArray(char[][]::new);
 
-        char[][] expandedGrid = expand(gardenGrid);
+        List<Map<Character, Set<Coordinates>>> groups = buildGroups(gardenGrid);
+        int total = 0;
+        for (Map<Character, Set<Coordinates>> group : groups) {
+            for (Map.Entry<Character, Set<Coordinates>> entry : group.entrySet()) {
+                char plant = entry.getKey();
+                Set<Coordinates> plants = entry.getValue();
+                int area = plants.size();
 
-        print(gardenGrid);
-        print(expandedGrid);
+                //one element expected only
+                int perimeter = calculatePerimeter(plants, plant, gardenGrid.length, gardenGrid[0].length);
 
-        Map<Character, Integer> areas = collectAreas(gardenGrid);
-        System.out.println(areas);
+                System.out.println("group " + plant + " " + area + " " + perimeter);
+                total+=area*perimeter;
+            }
 
-        int part1Result = part1(areas, gardenGrid);
-        System.out.println("Part 1 result " + part1Result);
+            System.out.println();
+
+        }
+
+        System.out.println(groups);
+
+
+//        char[][] expandedGrid = expand(gardenGrid);
+
+//        print(gardenGrid);
+//        print(expandedGrid);
+//
+//        Map<Character, Integer> areas = collectAreas(gardenGrid);
+//        System.out.println(areas);
+//
+//        int part1Result = part1(areas, gardenGrid);
+        System.out.println("Part 1 result " + total);
+    }
+
+    private static int calculatePerimeter(Set<Coordinates> plants, char plant, int i, int j) {
+        char[][] grid = new char[i][j];
+        for (Coordinates p : plants) {
+            grid[p.i][p.j] = plant;
+        }
+        char[][] expandedGrid = expand(grid);
+        return calculatePerimeter(plant, expandedGrid);
+    }
+
+    private static List<Map<Character, Set<Coordinates>>> buildGroups(char[][] gardenGrid) {
+        Set<Coordinates> visited = new HashSet<>();
+        List<Map<Character, Set<Coordinates>>> groups = new ArrayList<>();
+        for (int i = 0; i < gardenGrid.length; i++) {
+            for (int j = 0; j < gardenGrid[0].length; j++) {
+                Coordinates coordinates = new Coordinates(i, j);
+                if (!visited.contains(coordinates)) {
+                    char plant = gardenGrid[i][j];
+
+                    Map<Character, Set<Coordinates>> group = buildGroup(gardenGrid, plant,  i, j, visited);
+                    groups.add(group);
+                }
+            }
+        }
+        return groups;
+    }
+
+    private static Map<Character, Set<Coordinates>> buildGroup(char[][] gardenGrid, char plant, int i, int j ,Set<Coordinates> visited) {
+        Set<Coordinates> group = new HashSet<>();
+        buildGroup(gardenGrid,  plant,  i,  j ,group,  visited);
+        return Map.of(plant, group);
+    }
+
+    private static void buildGroup(char[][] gardenGrid, char plant, int i, int j, Set<Coordinates> group, Set<Coordinates> visited) {
+        Queue<Coordinates> queue = new LinkedList<>();
+        queue.add(new Coordinates(i, j));
+
+        while (!queue.isEmpty()) {
+            Coordinates poll = queue.poll();
+            // test boundries here
+            if (poll.i <0 || poll.j<0 || poll.i >= gardenGrid.length || poll.j >=gardenGrid[0].length) {
+                continue;
+            }
+            if (visited.contains(poll)) {
+                continue;
+            }
+
+            if (gardenGrid[poll.i][poll.j] == plant) {
+                group.add(poll);
+                visited.add(poll);
+                // add neighbours here
+                int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+                for (int[] direction : directions) {
+                   queue.add(new Coordinates(poll.i + direction[0], poll.j + direction[1]));
+                }
+            }
+        }
+
+
+        // if out of boundry return
+        if (gardenGrid[i][j] == plant) {
+            Coordinates coordinates = new Coordinates(i, j);
+            group.add(coordinates);
+            visited.add(coordinates);
+        }
+
+
     }
 
     private static int part1(Map<Character, Integer> areas, char[][] gardenGrid) {
@@ -41,9 +139,9 @@ public class GardenGroups {
         return total;
     }
 
-    private static int calculatePerimeter(char plant, char[][] gardenGrid) {
+    private static int calculatePerimeter(char plant, char[][] expanded) {
         System.out.println("calculate perimeter " + plant);
-        char[][] expanded = expand(gardenGrid);
+//        char[][] expanded = expand(gardenGrid);
 
         for (int i = 0; i < expanded.length; i++) {
             for (int j = 0; j < expanded[0].length; j++) {
@@ -52,7 +150,7 @@ public class GardenGroups {
                 }
             }
         }
-        print(expanded);
+//        print(expanded);
         int perimeterCount = 0;
         for (char[] chars : expanded) {
             for (char aChar : chars) {
