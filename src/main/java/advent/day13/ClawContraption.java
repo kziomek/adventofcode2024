7c3b3c08cf8a55762250f6d8b5e58a01b84bc795
Krzysztof Ziomek <kziomek@onet.eu>
2025-02-23 16:57:48 +0000

    AdventOfCode 2024 - day 13 part 2

--- src/main/java/advent/day13/ClawContraption.java
+++ src/main/java/advent/day13/ClawContraption.java
@@ -1,32 +1,34 @@
 package advent.day13;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.math.BigInteger;
 import java.util.List;
 
 public class ClawContraption {
 
     public static void main(String[] args) throws IOException {
-//        List<Machine> machines = Parser.parse("src/main/resources/day13/example.txt");
+        //        List<Machine> machines = Parser.parse("src/main/resources/day13/example.txt");
         List<Machine> machines = Parser.parse("src/main/resources/day13/my-input.txt");
 
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
         System.out.println(machines);
 
         for (Machine machine : machines) {
             System.out.println(machine.a());
             System.out.println(machine.b());
+            System.out.println(machine.cost());
             System.out.println();
         }
 
-        int cost = countCost(machines);
-        System.out.println("Result part 1 = " + cost);
+        BigInteger cost = countCost(machines);
+        System.out.println("Result part 2 = " + cost.toString());
     }
 
-    private static int countCost(List<Machine> machines) {
+    private static BigInteger countCost(List<Machine> machines) {
         return machines
             .stream()
             .map(Machine::cost)
-            .reduce(0, Integer::sum);
+            .reduce(BigInteger.ZERO, BigInteger::add);
     }
 }
--- src/main/java/advent/day13/Machine.java
+++ src/main/java/advent/day13/Machine.java
@@ -1,29 +1,33 @@
 package advent.day13;
 
+import java.math.BigInteger;
+
 public class Machine {
-    int axv;
-    int bxv;
-    int xp;
+    BigInteger axv;
+    BigInteger bxv;
+    BigInteger xp;
 
-    int ayv;
-    int byv;
-    int yp;
+    BigInteger ayv;
+    BigInteger byv;
+    BigInteger yp;
 
-    public Machine(int axv, int bxv, int xp, int ayv, int byv, int yp) {
-        this.axv = axv;
-        this.bxv = bxv;
-        this.xp = xp;
-        this.ayv = ayv;
-        this.byv = byv;
-        this.yp = yp;
+    public Machine(long axv, long bxv, long xp, long ayv, long byv, long yp) {
+        this.axv = BigInteger.valueOf(axv);
+        this.bxv = BigInteger.valueOf(bxv);
+        //        this.xp = BigInteger.valueOf(xp);
+        this.xp = BigInteger.valueOf(10000000000000L + xp);
+        this.ayv = BigInteger.valueOf(ayv);
+        this.byv = BigInteger.valueOf(byv);
+        //        this.yp = BigInteger.valueOf(yp);
+        this.yp = BigInteger.valueOf(10000000000000L + yp);
     }
 
-    public double a() {
-        return (double) (bxv * yp - byv * xp) / (bxv * ayv - byv * axv);
+    public BigInteger a() {
+        return (bxv.multiply(yp).subtract(byv.multiply(xp))).divide(bxv.multiply(ayv).subtract(byv.multiply(axv)));
     }
 
-    public double b() {
-        return (yp - ayv * a()) / byv;
+    public BigInteger b() {
+        return (yp.subtract(ayv.multiply(a()))).divide(byv);
     }
 
     @Override
@@ -38,14 +42,15 @@ public class Machine {
             '}';
     }
 
-    public int cost() {
+    public BigInteger cost() {
         if (!isValid()) {
-            return 0;
+            return BigInteger.ZERO;
         }
-        return 3 * (int) a() + (int) b();
+        return a().multiply(BigInteger.valueOf(3)).add(b());
     }
 
     public boolean isValid() {
-        return a() == (int) a() && b() == (int) b();
+        return a().multiply(axv).add(b().multiply(bxv)).equals(xp)
+            && a().multiply(ayv).add(b().multiply(byv)).equals(yp);
     }
 }
