418bd67d1bafcb8e730dac756b0fc23cf10bd3ab
Krzysztof Ziomek <kziomek@onet.eu>
2025-03-08 16:51:12 +0000

    AdventOfCode 2024 - day 14 part 2

--- src/main/java/advent/day14/RestroomRedoubt.java
+++ src/main/java/advent/day14/RestroomRedoubt.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class RestroomRedoubt {
 
     public static void main(String[] args) throws IOException {
-//        List<Robot> robots = Files.readAllLines(Path.of("src/main/resources/day14/example.txt"))
+        //        List<Robot> robots = Files.readAllLines(Path.of("src/main/resources/day14/example.txt"))
         List<Robot> robots = Files.readAllLines(Path.of("src/main/resources/day14/my-input.txt"))
             .stream()
             .map(Robot::parse)
@@ -16,22 +16,56 @@ public class RestroomRedoubt {
 
         int xBoundry = 101;
         int yBoundry = 103;
-        int moves = 100;
+        int moves = 100000;
 
-        int[][] posCount = posCount(yBoundry, xBoundry);
-        for (Robot robot : robots) {
-            robot.move(moves, xBoundry, yBoundry);
-            posCount[robot.sY][robot.sX]++;
-            System.out.println(robot);
+        long minSafetyFactor = Long.MAX_VALUE;
+        //        int result = 0;
+        for (int i = 0; i < moves; i++) {
+            for (Robot robot : robots) {
+                robot.move(1, xBoundry, yBoundry);
+            }
+            int[][] posCount = posCount(yBoundry, xBoundry);
+            for (Robot robot : robots) {
+                posCount[robot.sY][robot.sX]++;
+                //                System.out.println(robot);
+            }
+
+            long safetyFactor = safetyFactorPart2(posCount, xBoundry, yBoundry);
+
+            if (safetyFactor < minSafetyFactor) {
+                minSafetyFactor = safetyFactor;
+                System.out.println("i=" + i + "seconds lapsed " + (i + 1));
+                System.out.println("Min safety factor " + minSafetyFactor);
+                printPosCount(posCount);
+            }
         }
-
-        int result = safetyFactor(posCount, xBoundry, yBoundry);
-
-        System.out.println(result);
     }
 
-    private static int safetyFactor(int[][] posCount, int xBoundry, int yBoundry){
-        int q1 = 0, q2 = 0, q3 = 0, q4 = 0;
+    //        int[][] posCount = posCount(yBoundry, xBoundry);
+    //        for (Robot robot : robots) {
+    //            posCount[robot.sY][robot.sX]++;
+    //            System.out.println(robot);
+    //        }
+
+    //        System.out.println(result);
+    //}
+
+    private static void printPosCount(int[][] posCount) {
+        System.out.println("=========");
+        for (int[] ints : posCount) {
+            for (int anInt : ints) {
+                if (anInt == 0) {
+                    System.out.print('.');
+                } else {
+                    System.out.print(anInt);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static long safetyFactorPart2(int[][] posCount, int xBoundry, int yBoundry) {
+        long q1 = 0, q2 = 0, q3 = 0, q4 = 0;
         for (int i = 0; i < posCount.length; i++) {
             for (int j = 0; j < posCount[0].length; j++) {
                 int iHalf = (yBoundry - 1) / 2;
@@ -39,14 +73,43 @@ public class RestroomRedoubt {
                 if (i == iHalf || j == jHalf) {
                     continue;
                 }
+                if (posCount[i][j] == 0) {
+                    continue;
+                }
                 if (i < iHalf && j < jHalf) {
-                    q1 += posCount[i][j];
+                    q1++;
                 } else if (i < iHalf) {
-                    q2 += posCount[i][j];
+                    q2++;
                 } else if (j < jHalf) {
-                    q3 += posCount[i][j];
+                    q3++;
                 } else {
-                    q4 += posCount[i][j];
+                    q4++;
+                }
+            }
+        }
+        return q1 * q2 * q3 * q4;
+    }
+
+    private static long safetyFactor(int[][] posCount, int xBoundry, int yBoundry) {
+        long q1 = 0, q2 = 0, q3 = 0, q4 = 0;
+        for (int i = 0; i < posCount.length; i++) {
+            for (int j = 0; j < posCount[0].length; j++) {
+                int iHalf = (yBoundry - 1) / 2;
+                int jHalf = (xBoundry - 1) / 2;
+                if (i == iHalf || j == jHalf) {
+                    continue;
+                }
+                if (posCount[i][j] == 0) {
+                    continue;
+                }
+                if (i < iHalf && j < jHalf) {
+                    q1++;
+                } else if (i < iHalf) {
+                    q2++;
+                } else if (j < jHalf) {
+                    q3++;
+                } else {
+                    q4++;
                 }
             }
         }
